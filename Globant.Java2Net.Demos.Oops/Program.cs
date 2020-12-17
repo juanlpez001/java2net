dc3641bf6f684e3e6537f7f318b90ed86f26d7c2
juanlpez001 <53976110+juanlpez001@users.noreply.github.com>
2020-12-17 16:08:14 -0500

    Challenge done!
    Write a program to calculate vehicle parking charges based on vehicle number of wheels. Please Implement OOPS concepts to achieve this (Interfaces, Abstract Classes, Polymorphism, classes for Car, Bike, Truck, etc) .
    
    The program will receive as argument the number of wheels and depending on number of wheels it should create the parking to attend the type of vehicle according to to the argument:
    
    2 Wheels: BicycleParking
    
    4 Wheels: CarParking
    
    >4 Wheels: TruckParking
    
    Each parking implementation should implement the following methods
    
    FindVehicle():void
    
    print type of vehicle that is being find
    
    ParkVehicle():void 
    
    print type of vehicle that is being parked
    
    CalculateRate(int minutes):double 
    
    number of wheels * minutes
    
    2 wheels for bicycle
    
    4 wheels for a car
    
    18 wheels for a truck

---
+++ Globant.Java2Net.Demos.Oops/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+namespace Globant.Java2Net.Demos.Oops
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the metering administration");
+            int result;
+
+            Car car = new Car("HVC480");
+            Bike bike = new Bike("02542");
+            Truck truck = new Truck("BMW554");
+
+            do
+            {
+                Console.WriteLine("How many wheels will have the vehicle on your parking?");
+                int wheels = int.Parse(Console.ReadLine());
+                Console.WriteLine("10 minutes");
+                if (wheels == 2)
+                {
+                    bike.parking();
+                    bike.FindVehicle();
+                    bike.getPlate();
+                    bike.weels();
+                    bike.CalculateRate(10, 2);
+
+                }
+
+                if (wheels == 4)
+                {
+                    car.parking();
+                    car.FindVehicle();
+                    car.getPlate();
+                    car.weels();
+                    car.CalculateRate(10, 4);
+                }
+
+                if (wheels == 18)
+                {
+                    truck.parking();
+                    truck.FindVehicle();
+                    truck.getPlate();
+                    truck.weels();
+                    truck.CalculateRate(10, 18);
+                }
+
+                if (wheels != 2 & wheels != 4 & wheels != 18)
+                {
+                    Console.WriteLine("Error, Try Again");
+                }
+                Console.WriteLine("Continue?");
+                Console.WriteLine("1-Yes\n2-No?");
+                result = int.Parse(Console.ReadLine());
+            } while (result != 2);
+
+
+        }
+
+
+
+        internal interface IParkingFactory
+        {
+            int weels();
+
+        }
+
+        internal class Vehicle
+        {
+            public Vehicle(string plate)
+            {
+               plateVehicle = plate;
+
+            }
+            public virtual void parking()
+            {
+                Console.WriteLine("Parking");
+            }
+
+            public virtual void CalculateRate(int minutes, int weels)
+            {
+
+                Console.WriteLine("rate is: " + minutes * weels);
+            }
+
+            public void getPlate()
+            {
+                Console.WriteLine("The plate of the vehicle is: " + plateVehicle);
+            }
+
+            private readonly string plateVehicle;
+
+        }
+
+        internal class Bike : Vehicle, IParkingFactory
+        {
+            public Bike(string plateNumber) : base(plateNumber)
+            {
+
+
+            }
+
+            public override void parking()
+            {
+                Console.WriteLine("BicycleParking!!!");
+            }
+
+            public int weels()
+            {
+                return 2;
+            }
+
+            public void FindVehicle()
+            {
+                Console.WriteLine("Finding a Bicycle!!!");
+
+            }
+
+            public override void CalculateRate(int minutes, int weels)
+            {
+
+                Console.WriteLine("rate is: " + minutes * weels);
+            }
+
+        }
+
+        internal class Car : Vehicle, IParkingFactory
+        {
+            public Car(string plateNumber) : base(plateNumber)
+            {
+
+
+            }
+
+
+            public int weels()
+            {
+                return 4;
+            }
+
+            public override void parking()
+            {
+                Console.WriteLine("Parking a Car!!!");
+            }
+
+            public override void CalculateRate(int minutes, int weels)
+            {
+                Console.WriteLine("rate is: " + minutes * weels);
+            }
+            public void FindVehicle()
+            {
+                Console.WriteLine("Finding a Car!!!");
+
+            }
+        }
+
+        internal class Truck : Vehicle, IParkingFactory
+        {
+            public Truck(string plateNumber) : base(plateNumber)
+            {
+
+
+            }
+
+            public override void parking()
+            {
+                Console.WriteLine("TruckParking");
+            }
+
+
+            public int weels()
+            {
+                return 18;
+            }
+
+            public void FindVehicle()
+            {
+                Console.WriteLine("Finding a Truck!!!");
+
+            }
+            public override void CalculateRate(int minutes, int weels)
+            {
+                Console.WriteLine("rate is: " + minutes * weels);
+            }
+
+
+        }
+    }
+}
+
+
